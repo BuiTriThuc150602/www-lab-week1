58b41408338c6cd2c41fb732ddbc3b6c3ea268a7
BuiTriThuc150602 <20088361.thuc@student.iuh.edu.vn>
2023-09-09 14:38:00 +0700

    init project, structure

---
+++ src/main/java/vn/edu/iuh/fit/controllers/ControllerServlet.java
@@ -0,0 +1,26 @@
+package vn.edu.iuh.fit.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/ControllerServlet"})
+public class ControllerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        if (action.equalsIgnoreCase("insert_account")) {
+            resp.getWriter().println("insert your account");
+        }
+
+    }
+}
