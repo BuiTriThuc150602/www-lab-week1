ffe02c9de1dfe4c7d1951f14786a865aa2554e1d
BuiTriThuc150602 <20088361.thuc@student.iuh.edu.vn>
2023-09-16 01:57:36 +0700

    Update : add funtion grant role for user by admin

--- src/main/webapp/JS/insert_account.js
+++ src/main/webapp/JS/insert_account.js
@@ -1,54 +1,49 @@
-let us = document.getElementById("us").value;
-let pwd = document.getElementById("pwd").value;
-let email = document.getElementById("email").value;
-let phone = document.getElementById("phone").value;
 let btnSubmit = document.getElementById("btnSubmit");
 let erUS = document.getElementById("erUS");
 let erPWD = document.getElementById("erPwd");
 let erEmail = document.getElementById("erEmail");
 let erPhone = document.getElementById("erPhone");
-
-console.log(us);
-console.log(pwd);
-console.log(email);
-console.log(phone);
+// check validate
+let check = () => {
+  let us = document.getElementById("us").value;
+  let pwd = document.getElementById("pwd").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  console.log(us);
+  console.log(pwd);
+  console.log(email);
+  console.log(phone);
+  if (us === "") {
+    erUS.innerHTML = "Username is required";
+    erUS.style.display = "block";
+    return false;
+  } else {
+    erUS.style.display = "none";
+  }
+  if (pwd === "") {
+    erPWD.innerHTML = "Password is required";
+    erPWD.style.display = "block";
+    return false;
+  } else {
+    erPWD.style.display = "none";
+  }
+  if (email === "") {
+    erEmail.innerHTML = "Email is required";
+    erEmail.style.display = "block";
+    return false;
+  } else {
+    erEmail.style.display = "none";
+  }
+  if (phone === "") {
+    erPhone.innerHTML = "Phone is required";
+    erPhone.style.display = "block";
+    return false;
+  } else {
+    erPhone.style.display = "none";
+  }
+  return true;
+};
 btnSubmit.addEventListener("click", () => {
-  // check validate
-  let check = () => {
-    console.log(us);
-    console.log(pwd);
-    console.log(email);
-    console.log(phone);
-    if (us === "") {
-      erUS.innerHTML = "Username is required";
-      erUS.style.display = "block";
-      return false;
-    } else {
-      erUS.style.display = "none";
-    }
-    if (pwd === "") {
-      erPWD.innerHTML = "Password is required";
-      erPWD.style.display = "block";
-      return false;
-    } else {
-      erPWD.style.display = "none";
-    }
-    if (email === "") {
-      erEmail.innerHTML = "Email is required";
-      erEmail.style.display = "block";
-      return false;
-    } else {
-      erEmail.style.display = "none";
-    }
-    if (phone === "") {
-      erPhone.innerHTML = "Phone is required";
-      erPhone.style.display = "block";
-      return false;
-    } else {
-      erPhone.style.display = "none";
-    }
-    return true;
-  };
   if (check()) {
     console.log("ok");
     btnSubmit.type = "submit";
